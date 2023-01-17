b38c9346a7291d0c757be839d5a744680497695e
sabkar <karim13.sab@gmail.com>
2023-01-17 21:04:04 +0300

    React project editor

---
+++ editor/src/App.js
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Container>
+      Hello World
+    </Container>
+  );
+}
+
+export default App;
