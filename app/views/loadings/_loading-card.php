6ee0790d522167de77d05ddf7ef415ea110379f8
sabkar <karim13.sab@gmail.com>
2023-01-23 20:37:53 +0300

    Fix: card width and wrapping row

--- app/views/loadings/_loading-card.php
+++ app/views/loadings/_loading-card.php
@@ -9,32 +9,37 @@
 ?>
 
 <?= <<<HTML
-<div style="width: 18rem;" class='card mr-2 mb-2 $cardClassName'>
-  <div class="card-header color-success">
-    <div class="d-flex justify-content-between">
-      <span>Погрузка №$id от $creationDate</span>
-      <a role="menu" data-toggle="dropdown" style="cursor: pointer; user-select: none;">
-        ...
-        <div class="dropdown-menu" >
-          <a class="dropdown-item" href="#">Редактировать</a>
-          <a class="dropdown-item" href="#">Зарегистировать оплату</a>
-          <a class="dropdown-item" href="#">Груз отгружен</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Закрыть погрузку</a>
-        </div>
-      </a>
+<div class="col-4">
+  <div class='card mb-4 $cardClassName' >
+    <div class="card-header color-success">
+      <div class="d-flex justify-content-between">
+        <span>Погрузка №$id от $creationDate</span>
+        <a role="menu" data-toggle="dropdown" style="cursor: pointer; user-select: none;">
+          ...
+          <div class="dropdown-menu" >
+            <a class="dropdown-item" href="#">Редактировать</a>
+            <a class="dropdown-item" href="#">Зарегистировать оплату</a>
+            <a class="dropdown-item" href="#">Груз отгружен</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Закрыть погрузку</a>
+          </div>
+        </a>
+      </div>
     </div>
-  </div>
-  <div class="card-body">
-    <h6 class="card-subtitle mb-2 ">$transportName - $transportStateNumber</h6>
-    <p class="card-text">
-      Заказов <b>3</b> на сумму <b>4500 РУБ</b>.
-      Всего товаров <b>10</b> весом <b>550 Кг</b>.
-      Стоимость доставки: <b>500 РУБ</b>
-    </p>
-    <div class="d-flex justify-content-between">
-      <a href="#" class="card-link text-success">Оплачено</a>
-      <a href="#" class="card-link $shippedButtonClassName ">Не отгружено</a>
+    <div class="card-body">
+      <h6 class="card-subtitle mb-2 ">$transportName - $transportStateNumber</h6>
+      <p class="card-text">
+        Заказов <b>3</b> на сумму <b>4500 РУБ</b>.</br>
+        Всего товаров <b>10</b> весом <b>550 Кг</b>.
+        Стоимость доставки: <b>500 РУБ</b>
+      </p>
+      <div class="d-flex justify-content-between">
+        <a href="#" class="card-link text-success">Оплачено</a>
+        <a href="#" class="card-link $shippedButtonClassName ">Не отгружено</a>
+      </div>
+    </div>
+    <div class="card-footer btn text-center p-1">
+      Редактировать
     </div>
   </div>
 </div>
--- app/views/loadings/index.php
+++ app/views/loadings/index.php
@@ -33,74 +33,13 @@ Modal::begin([
 
 <?php Modal::end(); ?>
 
-<div class="d-flex align-top">
+<div class="d-flex align-items-start flex-wrap row">
 
   <?php foreach ($loadings as $loading) {
     echo $this->render('_loading-card', ['loading' => $loading]);
   }
   ?>
 
-  <div class="card border-success mr-2 mb-2" style="width: 18rem;">
-    <div class="card-header color-success">
-      <div class="d-flex justify-content-between">
-        <span>Погрузка №1 от 22.04.23</span>
-        <a role="menu" data-toggle="dropdown" style="cursor: pointer; user-select: none;">
-          ...
-          <div class="dropdown-menu" >
-            <a class="dropdown-item" href="#">Редактировать</a>
-            <a class="dropdown-item" href="#">Зарегистировать оплату</a>
-            <a class="dropdown-item" href="#">Груз отгружен</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Закрыть погрузку</a>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="card-body">
-      <h6 class="card-subtitle mb-2 ">ГАЗель NEXT - A 226 ТУ</h6>
-      <p class="card-text">
-        Заказов <b>3</b> на сумму <b>4500 РУБ</b>.
-        Всего товаров <b>10</b> весом <b>550 Кг</b>.
-        Стоимость доставки: <b>500 РУБ</b>
-      </p>
-      <div class="d-flex justify-content-between">
-        <a href="#" class="card-link text-success ">Отгружено</a>
-        <a href="#" class="card-link text-success">Оплачено</a>
-      </div>
-    </div>
-  </div>
-  
-  <div class="card border-dark mr-2 mb-2" style="width: 18rem;">
-    <div class="card-header color-success">
-      <div class="d-flex justify-content-between">
-        <span>Погрузка №1 от 22.04.23</span>
-        <a role="menu" data-toggle="dropdown" style="cursor: pointer; user-select: none;">
-          ...
-          <div class="dropdown-menu" >
-            <span class="dropdown-item-text">Dropdown item text</span>
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="card-body">
-      <h6 class="card-subtitle mb-2 ">ГАЗель NEXT - A 226 ТУ</h6>
-      <p class="card-text">
-        Заказов <b>3</b> на сумму <b>4500 РУБ</b>.
-        Всего товаров <b>10</b> весом <b>550 Кг</b>.
-        Стоимость доставки: <b>500 РУБ</b>
-      </p>
-      <div class="d-flex justify-content-between flex-wrap ">
-        <a href="#" class="card-link text-success ml-0">Отгружено</a>
-        <a href="#" class="card-link text-success ml-0">Оплачено</a>
-      </div>
-    </div>
-    <div class="card-footer btn text-center p-1">
-      Редактировать
-    </div>
-  </div>
 </div>
 
 <?php $js = <<<JS
